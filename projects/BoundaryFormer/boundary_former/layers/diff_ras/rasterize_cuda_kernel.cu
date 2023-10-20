59e20bc67f26c8bcd66debe48d3ec83b21800415
Kiyou Sen <49185386+MrkKSen@users.noreply.github.com>
2023-10-20 16:32:42 +0900

    Adapt to CUDA 11

--- projects/BoundaryFormer/boundary_former/layers/diff_ras/rasterize_cuda_kernel.cu
+++ projects/BoundaryFormer/boundary_former/layers/diff_ras/rasterize_cuda_kernel.cu
@@ -13,17 +13,17 @@ inline int GET_BLOCKS(const int N){
 
 // for the older gpus atomicAdd with double arguments does not exist
 #if  __CUDA_ARCH__ < 600 and defined(__CUDA_ARCH__)
-static __inline__ __device__ double atomicAdd(double* address, double val) {
-    unsigned long long int* address_as_ull = (unsigned long long int*)address;
-    unsigned long long int old = *address_as_ull, assumed;
-    do {
-        assumed = old;
-        old = atomicCAS(address_as_ull, assumed,
-                __double_as_longlong(val + __longlong_as_double(assumed)));
-    // Note: uses integer comparison to avoid hang in case of NaN (since NaN != NaN) } while (assumed != old);
-    } while (assumed != old);
-    return __longlong_as_double(old);
-}
+// static __inline__ __device__ double atomicAdd(double* address, double val) {
+//     unsigned long long int* address_as_ull = (unsigned long long int*)address;
+//     unsigned long long int old = *address_as_ull, assumed;
+//     do {
+//         assumed = old;
+//         old = atomicCAS(address_as_ull, assumed,
+//                 __double_as_longlong(val + __longlong_as_double(assumed)));
+//     // Note: uses integer comparison to avoid hang in case of NaN (since NaN != NaN) } while (assumed != old);
+//     } while (assumed != old);
+//     return __longlong_as_double(old);
+// }
 #endif
 
 #define GET_DIRECT_4d(data, x0, x1, x2, x3, sd0, sd1, sd2, sd3)         \
